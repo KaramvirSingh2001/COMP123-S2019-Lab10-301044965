1b15d25bcb60ecb1def86ff6506c200566b2bd0d
karamvir singh <singhkaramvir078@gmail.com>
2019-07-19 11:17:42 -0400

    Lab10 - aded end form and start form

--- Start Form/Program.cs
+++ Start Form/Program.cs
@@ -4,27 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace COMP123_S2019_Lab10_301044965
+namespace Start_Form
 {
     static class Program
     {
-        public static EndForm endform;
-        public static MainForm mainform;
-        public static StartForm startform;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        public static void Main()
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            mainform = new MainForm();
-            endform = new EndForm();
-            startform = new StartForm();
-            Application.Run(startform);
+            Application.Run(new StartForm());
         }
-
-    }  
+    }
 }
-
